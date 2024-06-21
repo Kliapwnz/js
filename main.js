6ce3ed8c6fa7db53dfac60d16ea8f8535d284846
Evgeniy Klimovch <kliasanik@gmail.com>
2024-06-21 14:53:04 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = "Николай";
+console.log("Привет, " + name);
+if (name.length > 6) {
+  console.log("Ну и длиннющее же у вас имя!");
+} else {
+  console.log("Имя у вас не из длинных.");
+}
